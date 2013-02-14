f302de24c61e67cfc9da44d1fc55327747113609
joshaber <joshaber@gmail.com>
2013-02-13 23:00:28 -0500

    use commands to simply this whole deal

--- iOSDemo/IMMViewController.m
+++ iOSDemo/IMMViewController.m
@@ -34,25 +34,6 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 
-	RACSignal *submit = [self.createButton rac_signalForControlEvents:UIControlEventTouchUpInside];
-	// Each time the button is clicked, do the network request.
-	@weakify(self);
-	RACSignal *requests = [[submit map:^(id _) {
-		@strongify(self);
-		return [[self doSomeNetworkStuff] catch:^(NSError *error) {
-			return [RACSignal return:error];
-		}];
-	}] replayLazily];
-	RACSignal *latestRequest = [requests switchToLatest];
-	RACSignal *resultSignal = [latestRequest filter:^ BOOL (id value) {
-		return [value isKindOfClass:NSNumber.class];
-	}];
-	RACSignal *errorSignal = [latestRequest filter:^ BOOL (id value) {
-		return [value isKindOfClass:NSError.class];
-	}];
-	RACSignal *error = [[RACSignal merge:@[ errorSignal, [resultSignal mapReplace:nil] ]] deliverOn:RACScheduler.mainThreadScheduler];
-	RACSignal *processing = [[RACSignal merge:@[ [requests mapReplace:@YES], [latestRequest mapReplace:@NO] ]] deliverOn:RACScheduler.mainThreadScheduler];
-
 	// Are all entries valid? This is derived entirely from the values of our UI.
 	RACSignal *formValid = [RACSignal
 		combineLatest:@[
@@ -65,28 +46,52 @@
 			return @(firstName.length > 0 && lastName.length > 0 && email.length > 0 && reEmail.length > 0 && [email isEqual:reEmail]);
 		}];
 
-	// The button's enabledness is derived from whether we're processing and
-	// whether our form is valid.
-	RACSignal *buttonEnabled = [RACSignal combineLatest:@[ [processing startWith:@NO], formValid ] reduce:^(NSNumber *processing, NSNumber *formValid) {
-		return @(!processing.boolValue && formValid.boolValue);
+	// Use a command to encapsulate the validity and in-flight check.
+	RACCommand *doNetworkStuff = [RACCommand commandWithCanExecuteSignal:formValid];
+	RACSignal *networkResults = [[[doNetworkStuff
+		addSignalBlock:^(id _) {
+			// Wait 3 seconds and then send a random YES/NO.
+			return [[[RACSignal interval:3] take:1] sequenceMany:^{
+				BOOL success = arc4random() % 2;
+				return [RACSignal return:@(success)];
+			}];
+		}]
+		// -addSignalBlock: returns a signal of signals. We only care about the
+		// latest (most recent).
+		switchToLatest]
+		deliverOn:RACScheduler.mainThreadScheduler];
+
+	RACSignal *submit = [self.createButton rac_signalForControlEvents:UIControlEventTouchUpInside];
+	[submit subscribeNext:^(id sender) {
+		[doNetworkStuff execute:sender];
 	}];
 
-	// The button's enabledness is driven by the `buttonEnabled` signal.
+	// Create a signal by KVOing the command's canExecute property. The signal
+	// starts with the current value of canExecute.
+	RACSignal *buttonEnabled = RACAbleWithStart(doNetworkStuff, canExecute);
+
+	// The button's enabledness is driven by whether the command can execute,
+	// which means that the form is valid and the command isn't already
+	// executing.
 	RAC(self.createButton.enabled) = buttonEnabled;
 
 	// The button's title color is driven by its enabledness.
 	UIColor *defaultButtonTitleColor = self.createButton.titleLabel.textColor;
 	RACSignal *buttonTextColor = [buttonEnabled map:^(NSNumber *x) {
-		return x.boolValue ? defaultButtonTitleColor : [UIColor lightGrayColor];
+		return x.boolValue ? defaultButtonTitleColor : UIColor.lightGrayColor;
 	}];
 
-	// Update the title color every our text color signal changes.
+	// Update the title color every our text color signal changes. We can't use
+	// the RAC macro since the only way to change the title color is by calling
+	// a multi-argument method. So we lift the selector into the RAC world
+	// instead.
 	[self.createButton rac_liftSelector:@selector(setTitleColor:forState:) withObjects:buttonTextColor, @(UIControlStateNormal)];
 
-	// Our fields' text color and enabledness is derived from whether we're
-	// processing.
-	RACSignal *fieldTextColor = [processing map:^(NSNumber *x) {
-		return x.boolValue ? [UIColor lightGrayColor] : [UIColor blackColor];
+	// Our fields' text color and enabledness is derived from whether our
+	// command is executing.
+	RACSignal *executing = [RACAble(doNetworkStuff, executing) deliverOn:RACScheduler.mainThreadScheduler];
+	RACSignal *fieldTextColor = [executing map:^(NSNumber *x) {
+		return x.boolValue ? UIColor.lightGrayColor : UIColor.blackColor;
 	}];
 
 	RAC(self.firstNameField.textColor) = fieldTextColor;
@@ -94,47 +99,39 @@
 	RAC(self.emailField.textColor) = fieldTextColor;
 	RAC(self.reEmailField.textColor) = fieldTextColor;
 
-	RACSignal *notProcessing = [processing map:^(NSNumber *x) {
+	RACSignal *notProcessing = [executing map:^(NSNumber *x) {
 		return @(!x.boolValue);
 	}];
-	
 	RAC(self.firstNameField.enabled) = notProcessing;
 	RAC(self.lastNameField.enabled) = notProcessing;
 	RAC(self.emailField.enabled) = notProcessing;
 	RAC(self.reEmailField.enabled) = notProcessing;
 
-	// Submission ends when we click and then finish processing.
-	RACSignal *submissionEnded = [[[submit mapReplace:processing] switchToLatest] filter:^ BOOL (NSNumber *processing) {
+	// Submission ends when the user clicks the button and then executing stops.
+	RACSignal *submissionEnded = [[[submit mapReplace:executing] switchToLatest] filter:^ BOOL (NSNumber *processing) {
 		return !processing.boolValue;
 	}];
 
-	// The submit count increments after the button's been clicked and we're
-	// done processing.
-	RACSignal *submitCount = [[RACSignal combineLatest:@[ submit, submissionEnded ]] scanWithStart:@0 combine:^(NSNumber *running, id _) {
+	// The submit count increments after submission has ended.
+	RACSignal *submitCount = [submissionEnded scanWithStart:@0 combine:^(NSNumber *running, id _) {
 		return @(running.integerValue + 1);
 	}];
 
-	// The status label only shows up after the first completed submission.
+	// Status label is hidden until after we've had a submission complete.
 	RAC(self.statusLabel.hidden) = [[submitCount startWith:@0] map:^(NSNumber *x) {
 		return @(x.integerValue < 1);
 	}];
 
-	// Status label text and color are driven by whether we got an error.
-	RAC(self.statusLabel.text) = [error map:^(id x) {
-		return x != nil ? NSLocalizedString(@"An error occurred!", @"") : NSLocalizedString(@"You're good!", @"");
+	// Derive the status label's text and color from our network result.
+	RAC(self.statusLabel.text) = [networkResults map:^(NSNumber *x) {
+		return x.boolValue ? NSLocalizedString(@"You're good!", @"") : NSLocalizedString(@"An error occurred!", @"");
 	}];
-	RAC(self.statusLabel.textColor) = [error map:^(id x) {
-		return x != nil ? [UIColor redColor] : [UIColor greenColor];
+	RAC(self.statusLabel.textColor) = [networkResults map:^(NSNumber *x) {
+		return x.boolValue ?  UIColor.greenColor : UIColor.redColor;
 	}];
 
-	RAC(UIApplication.sharedApplication, networkActivityIndicatorVisible) = processing;
-}
-
-- (RACSignal *)doSomeNetworkStuff {
-	return [[[[RACSignal interval:3] take:1] sequenceMany:^{
-		BOOL success = arc4random() % 2;
-		return success ? [RACSignal return:@YES] : [RACSignal error:[NSError errorWithDomain:@"" code:0 userInfo:nil]];
-	}] replayLazily];
+	// Keep the activity indicator up-to-date with our execution status.
+	RAC(UIApplication.sharedApplication, networkActivityIndicatorVisible) = executing;
 }
 
 @end
